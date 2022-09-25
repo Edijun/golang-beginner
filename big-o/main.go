c34a4a29c0f6e0fc9c5b6f317be31fba127d33ce
Edi Djunaidi <edijun.design@gmail.com>
2022-09-25 18:32:11 +0700

    measure with large array

--- big-o/main.go
+++ big-o/main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	var nemo = []string{"nemo"}
+	// var nemo = []string{"nemo"}
 
-	findNemo(nemo)
+	// var everyone = []string{"dory", "bruce", "marlin", "nemo", "gill", "bloat",
+	// "nigel", "squirt", "darla", "hank"}
+
+	// var large = new Array(99).fill("nemo")
+
+	var large = [10000000]string{"nemo"}
+
+	findNemo(large)
 }
 
-func findNemo(array []string) {
+func findNemo(array [10000000]string) {
+	t0 := time.Now()
 	for _, s := range array {
 		if s == "nemo" {
 			fmt.Println("Found NEMO!")
 		}
 	}
+	t1 := time.Now()
+	fmt.Printf("Call to find Nemo took %v (milliseconds)", t1.Sub(t0))
 }

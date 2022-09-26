ac5eb56c59d4de69bfee4aa8ea54aef8ae34aa74
Edi Djunaidi <edijun.design@gmail.com>
2022-09-26 21:41:37 +0700

    o(n)

--- big-o/main.go
+++ big-o/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
@@ -16,16 +15,13 @@ func main() {
 
 	var large = [10000000]string{"nemo"}
 
-	findNemo(large)
+	findNemo(large) // 0(n) --> Linear Time
 }
 
 func findNemo(array [10000000]string) {
-	t0 := time.Now()
 	for _, s := range array {
 		if s == "nemo" {
 			fmt.Println("Found NEMO!")
 		}
 	}
-	t1 := time.Now()
-	fmt.Printf("Call to find Nemo took %v (milliseconds)", t1.Sub(t0))
 }

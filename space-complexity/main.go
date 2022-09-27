97d751f49621aa4d9befd77d4af78c2736bbb32b
Edi Djunaidi <edijun.design@gmail.com>
2022-09-27 11:30:06 +0700

    o(n)

--- space-complexity/main.go
+++ space-complexity/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 
 	booooo([]int{1, 2, 3, 4, 5, 6})
+	fmt.Println(arrayOfHiNTimes(6))
 
 }
 
@@ -12,4 +13,12 @@ func booooo(n []int) {
 	for i := 0; i < len(n); i++ {
 		fmt.Println("boooooo!")
 	}
+}
+
+func arrayOfHiNTimes(n int) []string {
+	var hiArray = []string{}
+	for i := 0; i < n; i++ {
+		hiArray = append(hiArray, "hi")
+	}
+	return hiArray
 }
